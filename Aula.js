ebe8b2b999b94a66125a032324e5007032c9526b
getwes <wesleyfreire1707@gmail.com>
2022-02-16 07:03:34 -0300

    let , const

--- Aula.js
+++ Aula.js
@@ -4,7 +4,15 @@
 //console.log(altura);
 // cameCase
 
-let dia = 17;
-let mes = 7;
-let ano = 2002;
-console.log(dia,mes,ano);
+//let dia = 17;
+//let mes = 7;
+//let ano = 2002;
+//console.log(dia,mes,ano);
+
+  let valoringressoadulto = 20;
+  valoringressoadulto = 30;
+  console.log(valoringressoadulto)
+/// let permite poder redefinir o valor quantas vezes for necessário 
+   const valoringressoadulto = 20;
+   console.log(valoringressoadulto)
+// const é usado para quando o valor não precisa ser redefinido 
