0c365486494e2c4137a89eabdf179109b9fc8fe3
getwes <wesleyfreire1707@gmail.com>
2022-02-15 22:33:44 -0300

    variaveis

--- Aula.js
+++ Aula.js
@@ -1,2 +1,10 @@
-console.log('wesley')
-alert('mensagem')
+//let idade = 5;
+//console.log(idade);
+//let  altura = 180;
+//console.log(altura);
+// cameCase
+
+let dia = 17;
+let mes = 7;
+let ano = 2002;
+console.log(dia,mes,ano);
