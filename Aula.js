b26a0c91103d4c75d663f8f01c7d1d83dc8aecc1
getwes <wesleyfreire1707@gmail.com>
2022-02-17 07:24:15 -0300

    tipagem dinâmica

--- Aula.js
+++ Aula.js
@@ -22,3 +22,5 @@ let idade = 25; //number literal
 let estaAprovado = true; //boolean
 let sobrenome = undefined; // undefined
 let corSelecionada = null // redefinir um valor
+
+//Exemplo  'typeof nome' serve para mostrar em qual variável está.
