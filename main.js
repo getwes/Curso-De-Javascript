d2b4c6e42f8a5a1b50e62700130ad49be6c07d1a
getwes <wesleyfreire1707@gmail.com>
2022-04-20 20:21:54 -0300

    updating

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ const firstName = 'Wesley'
 const lastName = 'Freire'
 
 console.log('meu nome e ' + firstName +  ' ' + lastName)
+
+console.log(`Meu nome eh ${firstName.toUpperCase()} ${lastName.toLowerCase()}`)
