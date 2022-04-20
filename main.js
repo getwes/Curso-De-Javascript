2f50d3736e2f391d6f96b8de86e7fec499e17d6e
getwes <wesleyfreire1707@gmail.com>
2022-04-20 20:41:21 -0300

    updating

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-const number = 5
+//const number = 5
 
-console.log(number.toString());
-console.log(typeof number.toString())
+//console.log(number.toString());
+//console.log(typeof number.toString())
+
+//boleanos
+false 
+true
+
+console.log(3 == 3)
+
+// Null e umderfined
+
+const x = null;
+
+const y = undefined;
+
+const list =[1,2,3,4,5];
+const object ={name: 'wesley'}
