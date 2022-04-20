a79fdbb90a15012368dbbf7b6e0506855633356a
getwes <wesleyfreire1707@gmail.com>
2022-04-20 20:27:23 -0300

    updating

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 const firstName = 'Wesley'
 const lastName = 'Freire'
 
+const names = 'wesley,renan,vitinho'
+
 console.log('meu nome e ' + firstName +  ' ' + lastName)
 
 console.log(`Meu nome eh ${firstName.toUpperCase()} ${lastName.toLowerCase()}`)
+
+console.log(names.split(","))
