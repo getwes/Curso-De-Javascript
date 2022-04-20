5fdbe4df1b4ec8c69ffd6b46d95809fe2c2ec937
getwes <wesleyfreire1707@gmail.com>
2022-04-20 20:34:15 -0300

    updating

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-//strings
+const number = 5
 
-const firstName = 'Wesley'
-const lastName = 'Freire'
-
-const names = 'wesley,renan,vitinho'
-
-console.log('meu nome e ' + firstName +  ' ' + lastName)
-
-console.log(`Meu nome eh ${firstName.toUpperCase()} ${lastName.toLowerCase()}`)
-
-console.log(names.split(","))
+console.log(number.toString());
+console.log(typeof number.toString())
