cf6357c9a1164befa53f214e00eac25c53a55672
getwes <wesleyfreire1707@gmail.com>
2022-04-21 22:18:16 -0300

    update

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ const names =["wesley", "renan", "vitinho", 10, false];
 
 const renan = names[3];
 
-//names.push("joao");
+names.push("joao");
 
 names.unshift(20);
 
 names.pop();
+names.pop();
+names.pop();
+
+
+
+
+
 
 console.log(names);
